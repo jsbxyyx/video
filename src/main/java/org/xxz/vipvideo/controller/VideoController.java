3c14b366628f8418d1bd02ae3e3ebb5c8fd2fa56
tt <tt@tt>
2018-03-31 23:49:01 +0800

    增加play.html

--- src/main/java/org/xxz/vipvideo/controller/VideoController.java
+++ src/main/java/org/xxz/vipvideo/controller/VideoController.java
@@ -1,10 +1,11 @@
 package org.xxz.vipvideo.controller;
 
-import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.xxz.vipvideo.config.Result;
 import org.xxz.vipvideo.constant.CodeConstant;
 import org.xxz.vipvideo.exception.NotParseException;
@@ -18,13 +19,14 @@ import java.util.Objects;
  * @author tt
  */
 @Slf4j
-@RestController
+@Controller
 public class VideoController {
 
     @Resource
     private VideoService videoService;
 
     @RequestMapping(value = "/getUrl", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public Result getUrl(String url) {
         if (url.indexOf("sohu.com") > -1) {
             throw new NotParseException("无法解析");
@@ -34,6 +36,13 @@ public class VideoController {
             return Result.success("解析成功", pr.getUrl());
         }
         throw new NotParseException(pr.getMsg());
+    }
+
+    @RequestMapping(value = "/getUrl2")
+    public String getUrl2(String url, ModelMap model) {
+        Result r = getUrl(url);
+        model.put("url", r.getData());
+        return "play";
     }
 
 }
